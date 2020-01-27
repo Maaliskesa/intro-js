a680df32b287debfff589b64d13d22766cc07d26
Maaliskesa <57031908+Maaliskesa@users.noreply.github.com>
2020-01-27 14:33:39 +0200

    did functions complete

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,39 @@
     // Set event listeners
     function setEventListeners() {
         var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+        prev = document.getElementsByClassName('slider__button--prev')[0];
 
         next.addEventListener('click', getNext);
         prev.addEventListener('click', getPrev);
     }
 
     function getNext() {
-        // TODO
+        // TODO 
+        // slide = 0 has been given above
+        /* get items' elements with index of slide, remove active class from it
+        slide++ to increase the valua of slide by one
+        get items element with index of slide, and active class to it*/
+        
+        items[slide].classList.remove('active');
+        if (slide === totalItems - 1) {
+            slide = 0;
+        } else {
+            slide++;
+        }
+        items[slide].classList.add('active'); 
     }
 
-    function getPrev(){
-        // TODO      
+    function getPrev() {
+        // TODO  
+
+        items[slide].classList.remove('active');
+        if (slide === 0) {
+            slide = 4;
+        } else {
+            slide--;
+        }
+        items[slide].classList.add('active'); 
+
     }
 
     function initSlider() {
