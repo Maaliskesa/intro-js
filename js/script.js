b60dae20781e13c1aa80713b4d05b91d285d7567
Maaliskesa <57031908+Maaliskesa@users.noreply.github.com>
2020-02-13 14:31:07 +0200

    clock function added plus drop down boxes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+function moveArms() {
+    var now = new Date(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        hr = now.getHours(),
+        tick = 360/60;
+    
+    var secondArmPosition = tick * sec,
+        minuteArmPosition = tick * min + sec/60 * tick,
+        hourArmPosition = tick * 5 * hr + min/60 * 5;
+    
+    var SECONDHAND = document.querySelector('#second'),
+        HOURHAND = document.querySelector('#hour'),
+        MINUTEHAND = document.querySelector('#minute');
+
+    function update() {
+        secondArmPosition += tick;
+        minuteArmPosition += tick/60;
+        hourArmPosition += tick/60/60;
+
+        SECONDHAND.style.transform = 'rotate(' + secondArmPosition + 'deg)';
+        HOURHAND.style.transform = 'rotate(' + hourArmPosition + 'deg)';
+        MINUTEHAND.style.transform = 'rotate(' + minuteArmPosition + 'deg)';
+    };
+
+    update();
+    setInterval(update, 1000);
+}
+
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(moveArms);
+
+// var myMusic = document.getElementById("music");
+// function play() {
+//     myMusic.play();
+// }
+
+// function pause() {
+//     myMusic.pause();
+// }
